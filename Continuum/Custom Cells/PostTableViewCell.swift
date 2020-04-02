b94c37470c0c361213e84fde9bd0792caccb8936
gklyons <30358106+gklyons@users.noreply.github.com>
2020-04-01 22:47:39 -0600

    Post TVCell UI built and PostTableViewCell file created

---
+++ Continuum/Custom Cells/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Continuum
+//
+//  Created by Garrett Lyons on 4/1/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
